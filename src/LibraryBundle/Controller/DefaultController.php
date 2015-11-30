9a9996f8cd5dc2d29c83af30b70f21db2b1a62b2
danolife <badr.dany@gmail.com>
2015-11-30 15:06:17 +0100

    Started editing the views

--- src/LibraryBundle/Controller/DefaultController.php
+++ src/LibraryBundle/Controller/DefaultController.php
@@ -6,17 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+	public function indexAction()
     {
     	$em = $this->getDoctrine()->getManager();
     	$repo = $em->getRepository("LibraryBundle:Book");
     	$books = $repo->findAll();
+
         return $this->render('LibraryBundle:Default:index.html.twig', array('books' => $books));
     }
 
     public function categoryAction($slug)
     {
-    	return $this->render('LibraryBundle:Default:category.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository("LibraryBundle:Category");
+    	$category = $repo->findOneBy(array("slug" => $slug));
+    	$books = $category->getBooks();
+
+    	return $this->render('LibraryBundle:Default:category.html.twig', array('category' => $category, 'books' => $books));
     }
 
     public function bookAction($slug)
@@ -27,5 +33,15 @@ class DefaultController extends Controller
     public function authorAction($slug)
     {
     	return $this->render('LibraryBundle:Default:author.html.twig');
+    }
+
+    // liste les categories dans le menu
+    public function categoriesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository("LibraryBundle:Category");
+    	$categories = $repo->findAll();
+
+        return $this->render('LibraryBundle:Default:categories.html.twig', array('categories' => $categories));
     }
 }
