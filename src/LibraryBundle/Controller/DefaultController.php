195275a9a8644b8695f4fc7d614ea748bf79311a
danolife <badr.dany@gmail.com>
2015-12-02 16:35:32 +0100

    FOSUserBundle

---
+++ src/LibraryBundle/Controller/DefaultController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace LibraryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+	public function indexAction()
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository("LibraryBundle:Book");
+    	$books = $repo->findAll();
+
+        return $this->render('LibraryBundle:Default:index.html.twig', array('books' => $books));
+    }
+
+    public function categoryAction($slug)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository("LibraryBundle:Category");
+    	$category = $repo->findOneBySlug($slug);
+    	$books = $category->getBooks();
+
+    	return $this->render('LibraryBundle:Default:category.html.twig', array('category' => $category, 'books' => $books));
+    }
+
+    public function bookAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Book");
+        $book = $repo->findOneBySlug($slug);
+
+    	return $this->render('LibraryBundle:Default:book.html.twig', array('book' => $book));
+    }
+
+    public function authorAction($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Author");
+        $author = $repo->findOneBySlug($slug);
+        $books = $author->getBooks();
+
+    	return $this->render('LibraryBundle:Default:author.html.twig', array('author' => $author, 'books' => $books));
+    }
+
+    public function newsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Book");
+        $books = $repo->findByIsNew(1);
+        
+        return $this->render('LibraryBundle:Default:nouveautes.html.twig', array('books' => $books));
+    }
+
+    // liste les categories dans le menu
+    public function categoriesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository("LibraryBundle:Category");
+    	$categories = $repo->findAll();
+
+        return $this->render('LibraryBundle:Default:categories.html.twig', array('categories' => $categories));
+    }
+}
