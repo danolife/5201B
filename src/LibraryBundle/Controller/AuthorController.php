6934d392a649104cc85d52f2d471c3220773f15e
damienorny <damienorny@gmail.com>
2015-12-11 09:42:39 +0100

    vrai commit

--- src/LibraryBundle/Controller/AuthorController.php
+++ src/LibraryBundle/Controller/AuthorController.php
@@ -3,11 +3,76 @@
 namespace LibraryBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AuthorController extends Controller
 {
-    public function indexAction($name)
+    public function authorAction($slug)
     {
-        return $this->render('', array('name' => $name));
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Author");
+        $author = $repo->findOneBySlug($slug);
+        $books = $author->getBooks();
+
+        return $this->render('LibraryBundle:Default:author.html.twig', array('author' => $author, 'books' => $books));
+    }
+
+    public function addAuthorAction(Request $request)
+    {
+        $author = new Author();
+        $form = $this->get('form.factory')->create(new AuthorType(), $author);
+
+        if($form->handleRequest($request)->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Auteur bien ajouté');
+
+            return $this->redirect($this->generateUrl('library_author', array('slug'=>$author->getSlug())));
+        }
+
+        return $this->render('LibraryBundle:Default:addAuthor.html.twig', array('form'=>$form->createView()));
+    }
+
+    public function editAuthorAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Author");
+        $author = $repo->findOneBySlug($slug);
+        $form = $this->get('form.factory')->create(new AuthorType(), $author);
+
+        if($form->handleRequest($request)->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Auteur bien modifié');
+
+            return $this->redirect($this->generateUrl('library_author', array('slug'=>$author->getSlug())));
+        }
+
+        return $this->render('LibraryBundle:Default:addAuthor.html.twig', array('form'=>$form->createView()));
+    }
+
+    public function removeAuthorAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Author");
+        $author = $repo->findOneBySlug($slug);
+
+        if (null === $author) {
+            throw new NotFoundHttpException("Cet auteur n'existe pas");
+        }
+
+        $form = $this->createFormBuilder()->getForm();
+        if ($form->handleRequest($request)->isValid()) {
+            $em->remove($author);
+            $em->flush();
+            return $this->redirectToRoute('library_homepage');
+        }
+
+        return $this->render('LibraryBundle:Default:removeAuthor.html.twig', array('slug' => $slug, 'form' => $form->createView()));
     }
 }
--- src/LibraryBundle/Controller/BookController.php
+++ src/LibraryBundle/Controller/BookController.php
@@ -2,12 +2,84 @@
 
 namespace LibraryBundle\Controller;
 
+use LibraryBundle\Entity\Book;
+use LibraryBundle\Form\BookType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BookController extends Controller
 {
-    public function indexAction($name)
+    public function bookAction($slug)
     {
-        return $this->render('', array('name' => $name));
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Book");
+        $book = $repo->findOneBySlug($slug);
+        $bookInCart = false;
+        if($this->get('session')->has('cart') && $this->get('session')->get('cart')->getBooks()->contains($slug))
+        {
+            $bookInCart = true;
+        }
+        return $this->render('LibraryBundle:Default:book.html.twig', array('book' => $book, 'bookInCart' => $bookInCart));
+    }
+
+    public function editBookAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository("LibraryBundle:Book");
+        $book = $repo->findOneBySlug($slug);
+        $form = $this->get('form.factory')->create(new BookType(), $book);
+
+        if($form->handleRequest($request)->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Livre bien modifié');
+
+            return $this->redirect($this->generateUrl('library_book', array('slug'=>$book->getSlug())));
+        }
+
+        return $this->render('LibraryBundle:Default:addBook.html.twig', array('form'=>$form->createView()));
+    }
+
+    public function removeBookAction(Request $request, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $book = $em->getRepository("LibraryBundle:Book")->findOneBySlug($slug);
+
+        if (null === $book) {
+            throw new NotFoundHttpException("Ce livre n'existe pas");
+        }
+
+        $form = $this->createFormBuilder()->getForm();
+        if ($form->handleRequest($request)->isValid()) {
+            $em->remove($book);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('info', "Le livre a bien été supprimé.");
+
+            return $this->redirectToRoute('library_homepage');
+        }
+
+        return $this->render('LibraryBundle:Default:removeBook.html.twig', array('slug' => $slug, 'form' => $form->createView()));
+    }
+
+    public function addBookAction(Request $request)
+    {
+        $book = new Book();
+        $form = $this->get('form.factory')->create(new BookType(), $book);
+
+        if($form->handleRequest($request)->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Livre bien ajouté');
+
+            return $this->redirect($this->generateUrl('library_book', array('slug'=>$book->getSlug())));
+        }
+
+        return $this->render('LibraryBundle:Default:addBook.html.twig', array('form'=>$form->createView()));
     }
 }
